f22c87fc6c2a8a22c226ca9dc334f22e23cf6c4c
Uchuu Tamashi <ytc@mit.edu>
2014-10-09 18:02:00 -0400

    PlayMusic
    Minimalistic MP3 Player

---
+++ PlayMusic/PlayMusic/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NAudio;
+using NAudio.Wave;
+using System.Threading;
+namespace PlayMusic
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static bool EXITFLAG = false;
+        static IWavePlayer waveOutDevice;
+        static Thread AZUSA = new Thread(new ThreadStart(AZUSAListener));
+
+        static void Main(string[] args)
+        {
+
+
+            if (!File.Exists("mp3.txt"))
+            {
+                Console.WriteLine("{SearchFor(\"*.mp3\")}?");
+                string filepath = System.Web.HttpUtility.UrlDecode(Console.ReadLine());
+                Console.WriteLine("Reading from " + filepath);
+                if (!File.Exists("mp3.txt"))
+                {
+                    File.Move(filepath, "mp3.txt");
+                }
+                
+            }
+
+            AZUSA.Start();
+
+            string[] playlist = File.ReadAllLines("mp3.txt", Encoding.UTF8);
+
+            string filename;
+            WaveStream mp3Reader;
+            if (playlist.Count() > 0)
+            {
+                while (true)
+                {
+
+                    filename = playlist[rand.Next(playlist.Count())];
+                    mp3Reader = new Mp3FileReader(filename);
+                    if (mp3Reader.TotalTime.Minutes > 0)
+                    {
+                        PLAY(filename);
+                    }
+
+                }
+            }
+
+        }
+
+        static void AZUSAListener()
+        {
+            Console.WriteLine("LinkRID(PauseMusic,false)");
+            Console.WriteLine("LinkRID(StopMusic,false)");
+            string cmd;
+            while (true)
+            {
+                cmd = Console.ReadLine();
+                if (cmd == "PauseMusic()")
+                {
+                    if (waveOutDevice.PlaybackState == PlaybackState.Paused)
+                    {
+                        waveOutDevice.Play();
+                    }
+                    else
+                    {
+                        waveOutDevice.Pause();
+                    }
+
+                }
+                else if (cmd == "StopMusic()")
+                {
+                    EXITFLAG = true;
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        //Async MP3 Player
+        static void PLAY(string file)
+        {
+            //Declarations required for audio out and the MP3 stream
+
+            AudioFileReader audioFileReader;
+            waveOutDevice = new WaveOut();
+            audioFileReader = new AudioFileReader(file);
+            waveOutDevice.Init(audioFileReader);
+            waveOutDevice.Play();
+            waveOutDevice.PlaybackStopped += new EventHandler<StoppedEventArgs>(waveOutDevice_PlaybackStopped);
+
+            while (true)
+            {
+                if (!EXITFLAG)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        static void waveOutDevice_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            EXITFLAG = true;
+        }
+    }
+}
---
+++ SearchFor/SearchFor/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SearchFor
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static List<string> results = new List<string>();
+        static void Main(string[] args)
+        {
+            string target = "*.mp3";
+            if (args.Count() > 0)
+            {
+                target = args[0].Trim();
+            }
+
+            
+
+            foreach (string drive in Directory.GetLogicalDrives())
+            {               
+                
+                Search(drive,target);
+                
+            }
+
+            string resultfile=target.Split('/').Last().Replace("*","").Trim('.')+".txt";
+
+            File.WriteAllLines(resultfile,results, Encoding.UTF8);
+
+            Console.WriteLine("Return(\""+Path.GetFullPath(resultfile)+"\")");
+
+        }
+
+        static void Search(string dir, string target)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(dir, target, SearchOption.AllDirectories))
+                {
+                    results.Add(file);
+                }
+            }
+            catch
+            {
+                try
+                {
+                    foreach (string file in Directory.GetFiles(dir, target, SearchOption.TopDirectoryOnly))
+                    {
+                        results.Add(file);
+                    }
+
+                    Parallel.ForEach(Directory.GetDirectories(dir), directory =>
+                    {
+                        try
+                        {
+                            Search(directory, target);
+                        }
+                        catch { }
+                    });
+                    
+                }
+                catch { }
+            }            
+        }
+    }
+}
+
